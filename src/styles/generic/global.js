865b74a170a8a685d977aa65e2122083c6364bb2
Stefany <stefany.vasc.sa@gmail.com>
2020-12-07 20:42:44 -0300

    style: 💄 create folder and files for styles
    general styles and inside styles folder exist other folders (generic and settings)

---
+++ src/styles/generic/global.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import fonts from '../settings/fonts';
+
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    color: #333;
+
+    font-family: ${fonts.typeFirst}
+  }
+
+
+  img{
+    display: block;
+    max-width: 100%;
+  }
+
+  button, input{
+    display: block;
+    font-size: 1rem;
+    font-family: ${fonts.typeFirst};
+    color: #333;
+
+  }
+
+  .container{
+    max-width: 50rem;
+    padding: 0 1rem;
+    margin: 0 auto;
+  }
+`;
+
+export default GlobalStyles;
---
+++ src/styles/settings/fonts.js
@@ -0,0 +1,6 @@
+const fonts = {
+  typeFirst: 'Helvetica, Arial, sans-serif',
+  typeSecond: 'Spectral, Georgia',
+};
+
+export default fonts;
