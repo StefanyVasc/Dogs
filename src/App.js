2b14a5134a9e512bba6fcbe7b5062af7eaebb4a0
Stefany <stefany.vasc.sa@gmail.com>
2020-12-07 20:57:06 -0300

    feat: 🚧 start with Routes in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import GlobalStyles from './styles/generic/global';
 
 function App() {
   return (
     <div>
-      <h1>Olá</h1>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
