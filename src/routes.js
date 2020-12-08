b3a16a7a7aa2e8adf68063f5e0ba43fa50b901ba
Stefany <stefany.vasc.sa@gmail.com>
2020-12-08 18:23:20 -0300

    refactor: ♻️  change name of components main file

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login';
 
 export default function Rotas() {
   return (
