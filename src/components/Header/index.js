56ba2f3b7c5c0539886e8790b66cfba34b30dac6
Stefany <stefany.vasc.sa@gmail.com>
2020-12-15 18:17:10 -0300

    feat: ✨ create component UserStorage using Context Api e useContext Hook

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ReactComponent as Dogs } from '../../assets/dogs.svg';
+import { UserContext } from '../UserStorage/UserContext';
 import * as S from './styled';
 
 const Header = () => {
+  const { data } = useContext(UserContext);
+
   return (
     <S.HeaderWrapper>
       <S.HeaderNav as="nav">
         <S.HeaderLogoLink to="/" aria-label="Dogs - Home">
           <Dogs />
         </S.HeaderLogoLink>
-        <S.HeaderButton to="/login">Login | Cadastro</S.HeaderButton>
+        {data ? (
+          <S.HeaderButton to="/conta">{data.nome}</S.HeaderButton>
+        ) : (
+          <S.HeaderButton to="/login">Login | Cadastro</S.HeaderButton>
+        )}
       </S.HeaderNav>
     </S.HeaderWrapper>
   );
