32faa1c3e51fba6708fdafa4bd070172cb02d720
Stefany <stefany.vasc.sa@gmail.com>
2020-12-22 19:19:59 -0300

    feat: ✨ create page Account with subpages

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import FeedPhotos from '../../components/FeedPhotos';
+import { Layout } from '../../components/Layout/styled';
+import UserHeader from './UserHeader/UserHeader';
+import UserPhotoPost from './UserPhotoPost/UserPhotoPost';
+import UserStats from './UserStats/UserStats';
 
 const Account = () => {
-  return <div>área logada -- Usuário</div>;
+  return (
+    <Layout as="section">
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<FeedPhotos />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
+      </Routes>
+    </Layout>
+  );
 };
 
 export default Account;
