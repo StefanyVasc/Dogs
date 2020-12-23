7b92a111582ce0a3baced47e9e8f4b3e26df5e86
Stefany <stefany.vasc.sa@gmail.com>
2020-12-22 23:56:30 -0300

    feat: ✨ create component UserHeaderNav

--- src/pages/Account/UserHeader/UserHeaderNav/UserHeaderNav.js
+++ src/pages/Account/UserHeader/UserHeaderNav/UserHeaderNav.js
@@ -1,16 +1,16 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { ReactComponent as AddPhotos } from '../../../../assets/adicionar.svg';
 import { ReactComponent as MyStats } from '../../../../assets/estatisticas.svg';
 import { ReactComponent as MyPhotos } from '../../../../assets/feed.svg';
 import { ReactComponent as Logout } from '../../../../assets/sair.svg';
 import { UserContext } from '../../../../components/UserStorage/UserContext';
+import useMedia from '../../../../hooks/useMedia';
 import * as S from './styled';
 
-const UserHeaderNav = () => {
-  const [mobile, setMobile] = useState(null);
-  const { userLogout } = useContext(UserContext);
+const NavItens = ({ mobile, userLogout }) => {
   return (
-    <S.HeaderNav>
+    <>
       <S.NavLinkMenu to="/conta" end activeClassName>
         <MyPhotos />
         {mobile && 'Minhas Fotos'}
@@ -27,7 +27,44 @@ const UserHeaderNav = () => {
         <Logout />
         {mobile && 'Sair'}
       </button>
-    </S.HeaderNav>
+    </>
+  );
+};
+
+const UserHeaderNav = () => {
+  const { userLogout } = useContext(UserContext);
+  const mobile = useMedia('(max-width: 768px)');
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
+
+  return (
+    <>
+      {mobile && (
+        <S.MobileButton
+          aria-label="menu"
+          className={`mobileButton ${mobileMenu && 'mobileButtonActive'}`}
+          onClick={() => setMobileMenu(!mobileMenu)}
+        ></S.MobileButton>
+      )}
+
+      {mobile ? (
+        <S.NavMobile
+          className={`${mobile ? 'navMobile' : ''} ${
+            mobileMenu && 'navMobileActive'
+          }`}
+        >
+          <NavItens mobile={mobile} userLogout={userLogout} />
+        </S.NavMobile>
+      ) : (
+        <S.HeaderNav>
+          <NavItens userLogout={userLogout} />
+        </S.HeaderNav>
+      )}
+    </>
   );
 };
 
--- src/pages/Account/UserHeader/UserHeaderNav/styled.js
+++ src/pages/Account/UserHeader/UserHeaderNav/styled.js
@@ -30,6 +30,56 @@ export const HeaderNav = styled.nav`
   }
 `;
 
+export const NavMobile = styled(HeaderNav).attrs(({ customProperty }) => ({
+  customProperty,
+}))`
+  display: block;
+  grid-template-columns: none;
+  gap: none;
+  position: absolute;
+  top: 70px;
+  right: 0px;
+  padding: 0 1rem;
+  background: var(--default-color-light);
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
+  border-radius: 0.2rem;
+  transform: translateX(-10px);
+  opacity: 0;
+  pointer-events: none;
+
+  &.navMobileActive {
+    transition: 0.3s;
+    transform: initial;
+    opacity: 1;
+    pointer-events: initial;
+    z-index: 100;
+  }
+
+  button {
+    border-bottom: none;
+  }
+
+  svg {
+    margin-right: 0.5rem;
+  }
+
+  a,
+  button {
+    display: flex;
+    align-items: center;
+    background: none;
+    width: 100%;
+    border: none;
+    border-bottom: 1px solid #eee;
+    padding: 0.5rem 0;
+    cursor: pointer;
+
+    :hover svg > * {
+      fill: var(--primary-color);
+    }
+  }
+`;
+
 export const NavLinkMenu = styled(NavLink).attrs({ activeClassName })`
   &.${activeClassName} {
     background: var(--default-color-light);
@@ -39,6 +89,50 @@ export const NavLinkMenu = styled(NavLink).attrs({ activeClassName })`
 
     svg > * {
       fill: var(--primary-color);
+    }
+  }
+`;
+
+export const MobileButton = styled.button`
+  background: var(--quarternary-color);
+  border-radius: 0.2rem;
+  height: 40px;
+  width: 40px;
+  padding: 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid transparent;
+  transition: 0.1s;
+  cursor: pointer;
+
+  ::after {
+    content: '';
+    display: block;
+    width: 1.2rem;
+    height: 2px;
+    background: currentColor;
+    border-radius: 2px;
+    box-shadow: 0 6px currentColor, 0 -6px currentColor;
+    transition: 0.2s;
+  }
+
+  :hover,
+  :focus,
+  &.mobileButtonActive {
+    outline: none;
+    background: var(--default-color-light);
+    box-shadow: 0 0 0 3px var(--tertiary-color);
+    border-color: var(--primary-color);
+    color: var(--primary-color);
+  }
+
+  &.mobileButtonActive {
+    ::after {
+      transform: rotate(-90deg);
+      width: 4px;
+      height: 4px;
+      box-shadow: 0 8px currentColor, 0 -8px currentColor;
     }
   }
 `;
