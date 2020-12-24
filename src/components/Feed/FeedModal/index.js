110b9e2ec4b37b854f748d55bb03f77c321beb9f
Stefany <stefany.vasc.sa@gmail.com>
2020-12-24 00:12:07 -0300

    feat: ✨ create component FeedModal inside folder Feed

--- src/components/Feed/FeedModal/index.js
+++ src/components/Feed/FeedModal/index.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useFetch from '../../../hooks/useFetch';
+import { PHOTO_GET } from '../../../services/api';
+import Error from '../../Error';
+import Loading from '../../Loading';
+import PhotoContent from '../../Photo/PhotoContent';
+import * as S from './styled';
 
-const FeedModal = () => {
-  return <div>Feed modal</div>;
+const FeedModal = ({ photo, setModalPhoto }) => {
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  function handleOutsideClick(event) {
+    if (event.target === event.currentTarget) setModalPhoto(null);
+  }
+
+  return (
+    <S.Modal onClick={handleOutsideClick}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </S.Modal>
+  );
 };
 
 export default FeedModal;
---
+++ src/components/Feed/FeedModal/styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import media from 'styled-media-query';
+
+export const Modal = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.4);
+  top: 0px;
+  left: 0px;
+  display: flex;
+  z-index: 100;
+  padding: 2rem calc(4rem + 15px) 2rem 4rem;
+
+  ${media.lessThan('medium')`
+  padding: 2rem calc(2rem + 15px) 2rem 2rem;
+
+  `}
+`;
